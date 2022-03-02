11d928ce7c3003619e83486e77f1b435b109d0bb
dmitridev <42156228+dmitridev@users.noreply.github.com>
2022-03-02 16:47:28 +0300

    'done'

---
+++ app.js
@@ -0,0 +1,54 @@
+let isHex = true;
+document.getElementById("simple-color").style.color = "#777";
+let red = 0;
+let green = 0;
+let blue = 0;
+
+let randomizeColor = function (e) {
+    red = randomize();
+    green = randomize();
+    blue = randomize();
+    let color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    if (isHex)
+        color = `rgb(${red},${green},${blue})`;
+
+    document.getElementById('color').style.setProperty('background', color);
+    document.getElementById("color-flipper-color").innerHTML = `Цвет: ${color}`;
+    console.log(color);
+}
+
+randomizeColor();
+
+function randomize() {
+    return Math.floor(0 + Math.random() * 255);
+}
+
+document.getElementById("color-flipper-button").addEventListener('click', function (e) { randomizeColor(e) });
+document.getElementById("hex-color").addEventListener('click', function (e) {
+    console.log("here");
+    this.style.color = '#777';
+    document.getElementById("simple-color").style.color = "black";
+    isHex = true;
+    let color = `Цвет: #${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    document.getElementById("color-flipper-color").innerHTML = color;
+});
+
+document.getElementById("simple-color").addEventListener('click', function (e) {
+    this.style.color = '#777';
+    document.getElementById("hex-color").style.color = "black";
+    isHex = false;
+    let color = `Цвет: rgb(${red},${green},${blue})`;
+    document.getElementById("color-flipper-color").innerHTML = color;
+    console.log("hex-here");
+});
+
+
+document.getElementById("color-flipper-copy").addEventListener("click", function (e) {
+    let color=`rgb(${red},${green},${blue})`;
+    if(isHex){
+        color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    }
+    navigator.clipboard.writeText(color).then(function(){
+        console.log('successfully copied');
+    })
+});
