9e5edb10c9b85e6a45b9a823b3ed20c1c07b80e4
dmitridev <42156228+dmitridev@users.noreply.github.com>
2022-03-02 17:49:18 +0300

    fix service worker

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('service-worker.js',{scope: './'});
+    navigator.serviceWorker.register('service-worker.js');
 }
 
 let isHex = true;
