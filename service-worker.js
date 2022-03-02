f2cb118e9fc9e77f5323c9445fe2e40fc373cd6c
dmitridev <42156228+dmitridev@users.noreply.github.com>
2022-03-02 19:08:05 +0300

    remove images folder from links

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ self.addEventListener('install', (event) =>
     event.waitUntil(
         caches.open('v2').then((cache) =>
             cache.addAll([
-                './images/',
                 './app.css',
                 './index.html',
                 './app.js'
