aab8139a0aa968ab1f237f31da23f43a19d58aff
dmitridev <42156228+dmitridev@users.noreply.github.com>
2022-03-02 21:56:38 +0300

    fix service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,32 @@
-self.addEventListener('install', (event) =>
+self.addEventListener('install', (event) => {
+    //если в кзше уже есть такие вещи то тогда не загружать
     event.waitUntil(
         caches.open('v2').then((cache) =>
             cache.addAll([
                 'app.css',
                 'index.html',
-                'app.js'
+                'app.js',
+                '/images/favicon.png',
+                '/images/favicon-32x32.png',
+                '/images/favicon-192x192.png',
+                '/images/favicon-512x512.png'
             ])
         )
     )
+}
 );
 
-self.addEventListener('fetch', () => console.log("fetch event"));
+self.addEventListener('fetch', event => {
+    console.log('fetch: ', event.request.url);
+    event.respondWith(cacheFirst(event.request))
+}
+);
 
 self.addEventListener('activate', event => {
     console.log('[SW] : activate');
 })
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request);
+    return cached ?? await fetch(request);
+}
