fa53bf31602e1ba600104cd98f182e1d0eaa5481
dmitridev <42156228+dmitridev@users.noreply.github.com>
2022-03-04 13:52:21 +0300

    add animations and change accent color

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-if (!localStorage.getItem("isHex"))
-    localStorage.setItem("isHex", 'true');
-
-const accentColor = "#777";
-
-let isHex = localStorage.getItem('isHex') === "true";
-const str = isHex ? "hex-color" : "simple-color";
-document.getElementById(str).style.color = accentColor;
-
 let red = 0;
 let green = 0;
 let blue = 0;
+let colorFlipperButton = document.getElementById("color-flipper-button");
+let hexColor = document.getElementById("hex-color");
+let simpleColor = document.getElementById("simple-color");
+const accentColor = "black";
+const nonAccentColor = '#777';
 
-let randomizeColor = function (e) {
+let randomizeColor = function () {
+    function randomize() {
+        return Math.floor(0 + Math.random() * 255);
+    }
+
     red = randomize();
     green = randomize();
     blue = randomize();
     let color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+
     if (isHex)
         color = `rgb(${red},${green},${blue})`;
 
@@ -27,37 +28,47 @@ let saveToLocalStorage = function (key = 'isHex', value = true) {
     localStorage.setItem(key, value.toString());
 }
 
+
+
+
+// code goes here
+
+if (!localStorage.getItem("isHex"))
+    localStorage.setItem("isHex", 'true');
+
+
+let isHex = localStorage.getItem('isHex') === "true";
+const str = isHex ? "hex-color" : "simple-color";
+document.getElementById(str).style.color = accentColor;
+
+
+
 randomizeColor();
+colorFlipperButton.addEventListener('click', randomizeColor);
 
-function randomize() {
-    return Math.floor(0 + Math.random() * 255);
-}
-
-document.getElementById("color-flipper-button").addEventListener('click', function (e) { randomizeColor(e) });
-document.getElementById("hex-color").addEventListener('click', function (e) {
+hexColor.onclick = function (e) {
     this.style.color = accentColor;
-    document.getElementById("simple-color").style.color = "black";
+    document.getElementById("simple-color").style.color = nonAccentColor;
     isHex = true;
     saveToLocalStorage();
     let color = `Цвет: #${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
     document.getElementById("color-flipper-color").innerHTML = color;
-});
+}
 
-document.getElementById("simple-color").addEventListener('click', function (e) {
+simpleColor.onclick = function (e) {
     this.style.color = accentColor;
-    document.getElementById("hex-color").style.color = "black";
+    document.getElementById("hex-color").style.color = nonAccentColor;
     isHex = false;
     saveToLocalStorage('isHex', false);
     let color = `Цвет: rgb(${red},${green},${blue})`;
     document.getElementById("color-flipper-color").innerHTML = color;
-});
-
+}
 
 document.getElementById("color-flipper-copy").addEventListener("click", function (e) {
     let color = `rgb(${red},${green},${blue})`;
-    if (isHex) {
+
+    if (isHex)
         color = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
-    }
 
     navigator.clipboard.writeText(color).then(function () {
 
